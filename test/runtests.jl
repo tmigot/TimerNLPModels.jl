21f737740f18f36f94cb252d13810e3a61788cc9
tmigot <tangi.migot@gmail.com>
2024-06-28 23:13:42 +0200

    Add NLS API

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,19 +2,17 @@ using TimerNLPModels
 using NLPModels, NLPModelsTest
 using Test
 
-@testset "TimerNLPModels.jl" begin
+@testset "TimerNLPModels.jl for NLP" begin
   @testset "NLP: $pb" for pb in NLPModelsTest.nlp_problems
     nlp = TimerNLPModel(eval(Meta.parse(pb))())
     consistent_nlps([nlp, nlp]; exclude = [], linear_api = true)
   end
 end
 
-#=
 @testset "TimerNLPModels.jl for NLS" begin
   @testset "NLS: $pb" for pb in NLPModelsTest.nls_problems
-    nls = TimerNLPModel(eval(Meta.parse(pb))())
-    consistent_nlps([nls, nls], exclude = [], linear_api = true)
-    consistent_nlss([nls, nls], exclude = [], linear_api = true)
+    exclude = pb == "LLS" ? [hess_coord, hess] : []
+    nls = TimerNLSModel(eval(Meta.parse(pb))())
+    consistent_nlss([nls, nls], exclude = exclude, linear_api = true)
   end
 end
-=#
