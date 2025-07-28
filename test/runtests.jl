b5cefef4a6dd7fceee9e1c4106ddd5c3ea49d6e5
tmigot <tangi.migot@gmail.com>
2025-07-28 17:27:55 -0400

    Parallelize tests and fix consistency checks

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,29 @@
-using TimerNLPModels
-using NLPModels, NLPModelsTest
-using Test
+using Distributed
 
-@testset "TimerNLPModels.jl for NLP" begin
-  @testset "NLP: $pb" for pb in NLPModelsTest.nlp_problems
-    nlp = TimerNLPModel(eval(Meta.parse(pb))())
-    consistent_nlps([nlp, nlp]; exclude = [], linear_api = true)
+np = Sys.CPU_THREADS
+addprocs(np - 1)
+
+@everywhere using TimerNLPModels
+@everywhere using CUDA, NLPModels, NLPModelsTest, Test
+
+@everywhere function nlp_tests(p)
+  @testset "TimerNLPModels.jl for NLP: $p" begin
+    nlp = eval(Meta.parse(p))()
+    timed_nlp = TimerNLPModel(nlp)
+    consistent_nlps([nlp, timed_nlp]; exclude = [], linear_api = true)
   end
 end
-
-@testset "TimerNLPModels.jl for NLS" begin
-  @testset "NLS: $pb" for pb in NLPModelsTest.nls_problems
-    exclude = pb == "LLS" ? [hess_coord, hess] : []
-    nls = TimerNLSModel(eval(Meta.parse(pb))())
-    consistent_nlss([nls, nls]; exclude = exclude, linear_api = true)
+@everywhere function nls_tests(p)
+  @testset "TimerNLPModels.jl for NLS: $p" begin
+    nls = eval(Meta.parse(p))()
+    timed_nls = TimerNLSModel(nls)
+    exclude = p == "LLS" ? [hess_coord, hess] : []
+    consistent_nlss([nls, timed_nls]; exclude = exclude, linear_api = true)
   end
+end
+@testset "TimerNLPModels.jl for NLP" begin
+  pmap(nlp_tests, NLPModelsTest.nlp_problems)
+end
+@testset "TimerNLPModels.jl for NLS" begin
+  pmap(nls_tests, NLPModelsTest.nls_problems)
 end
