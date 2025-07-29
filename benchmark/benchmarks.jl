518754b7cabc436bd9f0b75cb731f34635462a84
tmigot <tangi.migot@gmail.com>
2025-07-29 09:16:40 -0400

    add NLPModels

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,4 +1,4 @@
-using BenchmarkTools, ADNLPModels
+using BenchmarkTools, ADNLPModels, NLPModels
 using OptimizationProblems
 using TimerNLPModels
 
@@ -6,12 +6,17 @@ using TimerNLPModels
 const SUITE = BenchmarkGroup()
 
 for n in [100, 1000]
-    g = zeros(n)
-    SUITE["grad! ref"]["$n"] = @benchmarkable grad!(nlp, get_x0(nlp), $g) setup = (nlp = OptimizationProblems.ADNLPProblems.arglina(n = $n))
-    SUITE["grad! tim"]["$n"] = @benchmarkable grad!(timed_nlp, get_x0(timed_nlp), $g) setup = (timed_nlp = TimerNLPModel(OptimizationProblems.ADNLPProblems.arglina(n = $n)))
+  g = zeros(n)
+  SUITE["grad! ref"]["$n"] = @benchmarkable grad!(nlp, get_x0(nlp), $g) setup =
+    (nlp = OptimizationProblems.ADNLPProblems.arglina(n = $n))
+  SUITE["grad! tim"]["$n"] = @benchmarkable grad!(timed_nlp, get_x0(timed_nlp), $g) setup =
+    (timed_nlp = TimerNLPModel(OptimizationProblems.ADNLPProblems.arglina(n = $n)))
 end
 for n in [100, 1000]
-    Hv = zeros(n)
-    SUITE["hprod! ref"]["$n"] = @benchmarkable hprod!(nlp, get_x0(nlp), get_x0(nlp), $Hv) setup = (nlp = OptimizationProblems.ADNLPProblems.arglina(n = $n))
-    SUITE["hprod! tim"]["$n"] = @benchmarkable hprod!(timed_nlp, get_x0(timed_nlp), get_x0(timed_nlp), $Hv) setup = (timed_nlp = TimerNLPModel(OptimizationProblems.ADNLPProblems.arglina(n = $n)))
+  Hv = zeros(n)
+  SUITE["hprod! ref"]["$n"] = @benchmarkable hprod!(nlp, get_x0(nlp), get_x0(nlp), $Hv) setup =
+    (nlp = OptimizationProblems.ADNLPProblems.arglina(n = $n))
+  SUITE["hprod! tim"]["$n"] =
+    @benchmarkable hprod!(timed_nlp, get_x0(timed_nlp), get_x0(timed_nlp), $Hv) setup =
+      (timed_nlp = TimerNLPModel(OptimizationProblems.ADNLPProblems.arglina(n = $n)))
 end
