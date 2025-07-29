545df5dd41f20e1c9829c3ce0fcdaacf409b1c6f
tmigot <tangi.migot@gmail.com>
2025-07-28 20:42:00 -0400

    Add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -27,3 +27,54 @@ end
 @testset "TimerNLPModels.jl for NLS" begin
   pmap(nls_tests, NLPModelsTest.nls_problems)
 end
+
+io = IOBuffer();
+map(
+  nlp -> print_nlp_allocations(io, nlp; linear_api = true),
+  map(x -> eval(Symbol(x))(), NLPModelsTest.nlp_problems),
+)
+print_nlp_allocations(io, LLS(); linear_api = true, exclude = [hess])
+map(
+  nlp -> print_nlp_allocations(io, nlp; linear_api = true),
+  map(x -> eval(Symbol(x))(), setdiff(NLPModelsTest.nls_problems, ["LLS"])),
+)
+
+#=
+map(
+  nlp -> test_zero_allocations(nlp; linear_api = true),
+  map(x -> TimerNLPModel(eval(Symbol(x))()), NLPModelsTest.nlp_problems),
+)
+test_zero_allocations(LLS(); linear_api = true, exclude = [hess])
+map(
+  nlp -> test_zero_allocations(nlp; linear_api = true),
+  map(x -> TimerNLSModel(eval(Symbol(x))()), setdiff(NLPModelsTest.nls_problems, ["LLS"])),
+)
+=#
+
+if CUDA.functional()
+  @everywhere function nlp_gpu_tests(p)
+    @testset "NLP tests of problem $p" begin
+      nlp_from_T = T -> TimerNLPModel(eval(Symbol(p))(T))
+      @testset "GPU multiple precision support of problem $p" begin
+        CUDA.allowscalar() do
+          multiple_precision_nlp_array(nlp_from_T, CuArray, linear_api = true, exclude = [])
+        end
+      end
+    end
+  end
+
+  @everywhere function nls_gpu_tests(p)
+    @testset "NLS tests of problem $p" begin
+      nls_from_T = T -> TimerNLSModel(eval(Symbol(p))(T))
+      exclude = p == "LLS" ? [hess_coord, hess] : []
+      @testset "GPU multiple precision support of problem $p" begin
+        CUDA.allowscalar() do
+          multiple_precision_nls_array(nls_from_T, CuArray, linear_api = true, exclude = exclude)
+        end
+      end
+    end
+  end
+
+  pmap(nlp_gpu_tests, NLPModelsTest.nlp_problems)
+  pmap(nls_gpu_tests, NLPModelsTest.nls_problems)
+end
